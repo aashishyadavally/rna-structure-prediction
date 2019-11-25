cf418b3e253ec74c8a7cfea15b3ad1ac708509a2
aashishyadavally <aashish.yadavally1995@gmail.com>
2019-11-25 17:49:21 -0500

    Add stable flag

--- structure_prediction/__main__.py
+++ structure_prediction/__main__.py
@@ -8,6 +8,7 @@ Aashish Yadavally
 
 import argparse
 from structure_prediction import *
+from structure_prediction_stacked import *
 
 
 def main():
@@ -18,7 +19,7 @@ def main():
 		'--energy',
 		type=str,
 		dest='energy',
-		required=True,
+		default='False',
 		choices=['True', 'False'],
 		help='If True, returns specific gamma-scores for each base-pair. '\
 			 'If False, returns 1 for each base-pair.',
@@ -30,17 +31,28 @@ def main():
 		dest='gap',
 		help='Number of bases between two nucleotide bases in sequence.'
 	)
+	parser.add_argument(
+		'--stable',
+		type=str,
+		default='False',
+		dest='stable',
+		choices=['True', 'False'],
+		help='If True, predicts stable RNA structure.'
+	)
 
 	args = parser.parse_args()
 	if args.energy == 'True':
 		legal_pairs = LEGAL_PAIRS_ENERGY
-		energy_argument = True
 	else:
 		legal_pairs = LEGAL_PAIRS
-		energy_argument = False
 
 	sequence = read_structure()
-	ps = PredictStructure(sequence, legal_pairs, energy_argument, args.gap)
+	if args.stable == 'False':
+		ps = PredictStructure(sequence, legal_pairs, args.gap)
+	else:
+		legal_pair_matrix = LEGAL_PAIR_MATRIX
+		pair_matrix_mapping = PAIR_MATRIX_MAPPING
+		ps = PredictStableStructure(sequence, legal_pair_matrix, pair_matrix_mapping)
 
 
 if __name__ == '__main__':
